fe862f72f47ba9e763ff5ba45fd93986c90567c0
Silly_coder <maityp394@gmail.com>
2026-04-16 16:01:06 +0530

    feat: initial commit

---
+++ app/(auth)/_components/loginForm.tsx
@@ -0,0 +1,138 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { loginSchema, LoginInput } from "@/lib/schemas";
+import { request } from "@/lib/fetcher";
+import { Field, FieldGroup, FieldLabel, FieldSet } from "@/components/ui/field";
+import { PasswordInput } from "@/components/ui/passwordInput";
+import { Loader2 } from "lucide-react";
+import { API } from "@/lib/api";
+import { useTransition } from "react";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<LoginInput>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: LoginInput) => {
+    startTransition(async () => {
+      const res = await request<{ user?: unknown; token?: string }>(
+        API.login,
+        "POST",
+        { data: values },
+        "Failed to login",
+      );
+
+      if (!res.success) {
+        toast.error(res.errMsg);
+        return;
+      }
+
+      console.log("login: successful, redirecting to tasks page...");
+      toast.success(res.message || "Login successful!");
+      router.replace("/tasks");
+    });
+  };
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0">
+        <CardContent className="p-0">
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="p-6 md:p-8 w-full"
+          >
+            <FieldGroup>
+              <FieldSet>
+                <div className="flex flex-col gap-6">
+                  <div className="flex flex-col items-center text-center">
+                    <h1 className="text-2xl font-bold">Welcome back</h1>
+                    <p className="text-muted-foreground text-balance">
+                      Login to your Primetrade account
+                    </p>
+                  </div>
+
+                  {/* Email */}
+                  <Field>
+                    <FieldLabel htmlFor="email">Email</FieldLabel>
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="m@example.com"
+                      autoFocus
+                      {...form.register("email")}
+                    />
+                    {form.formState.errors.email && (
+                      <p className="text-sm text-destructive">
+                        {form.formState.errors.email.message as string}
+                      </p>
+                    )}
+                  </Field>
+
+                  {/* Password */}
+                  <Field>
+                    <PasswordInput
+                      id="password"
+                      placeholder="••••••••"
+                      {...form.register("password")}
+                    />
+                    {form.formState.errors.password && (
+                      <p className="text-sm text-destructive">
+                        {form.formState.errors.password.message as string}
+                      </p>
+                    )}
+                  </Field>
+
+                  <div className="flex justify-end">
+                    <Button
+                      type="submit"
+                      className="w-full cursor-pointer"
+                      disabled={form.formState.isSubmitting || isPending}
+                    >
+                      {form.formState.isSubmitting || isPending ? (
+                        <span className="flex items-center justify-center gap-2">
+                          <Loader2 className="h-4 w-4 animate-spin" />
+                          Logging in...
+                        </span>
+                      ) : (
+                        "Login"
+                      )}
+                    </Button>
+                  </div>
+
+                  <div className="text-center text-sm">
+                    Don&apos;t have an account?{" "}
+                    <Link
+                      href="/signup"
+                      className="underline underline-offset-4"
+                    >
+                      Sign up
+                    </Link>
+                  </div>
+                </div>
+              </FieldSet>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
---
+++ app/(auth)/_components/signupForm.tsx
@@ -0,0 +1,162 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { SignupInput } from "@/lib/schemas";
+import { signupSchema } from "@/lib/schemas";
+import { useTransition } from "react";
+import { request } from "@/lib/fetcher";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Field, FieldGroup, FieldLabel, FieldSet } from "@/components/ui/field";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { PasswordInput } from "@/components/ui/passwordInput";
+import { Loader2 } from "lucide-react";
+import { API } from "@/lib/api";
+
+type UserResponse = {
+  username: string;
+  email: string;
+  id: string;
+};
+
+export function SignUpForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const router = useRouter();
+
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<SignupInput>({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit = (values: SignupInput) => {
+    startTransition(async () => {
+      const res = await request<{ user: UserResponse }>(
+        API.signup,
+        "POST",
+        { data: values },
+        "Failed to sign up",
+      );
+
+      if (!res.success) {
+        toast.error(res.errMsg);
+        return;
+      }
+
+      console.log("signup: successful, redirecting to tasks page...");
+      toast.success(res.message || "Successfully signed up!");
+      router.replace("/tasks");
+    });
+  };
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0">
+        <CardContent className="p-0">
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="p-6 md:p-8 w-full"
+          >
+            <FieldGroup>
+              <FieldSet>
+                <div className="flex flex-col gap-6">
+                  <div className="flex flex-col items-center text-center">
+                    <h1 className="text-2xl font-bold">Create your account</h1>
+                    <p className="text-muted-foreground text-balance">
+                      Sign up to start using Primetrade
+                    </p>
+                  </div>
+
+                  {/* Username */}
+                  <Field>
+                    <FieldLabel htmlFor="username">Username</FieldLabel>
+                    <Input
+                      id="username"
+                      placeholder="Your username"
+                      autoFocus
+                      {...form.register("username")}
+                    />
+                    {form.formState.errors.username && (
+                      <p className="text-sm text-destructive">
+                        {form.formState.errors.username.message as string}
+                      </p>
+                    )}
+                  </Field>
+
+                  {/* Email */}
+                  <Field>
+                    <FieldLabel htmlFor="email">Email</FieldLabel>
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="m@example.com"
+                      {...form.register("email")}
+                    />
+                    {form.formState.errors.email && (
+                      <p className="text-sm text-destructive">
+                        {form.formState.errors.email.message as string}
+                      </p>
+                    )}
+                  </Field>
+
+                  {/* Password */}
+                  <Field>
+                    <FieldLabel htmlFor="password">Password</FieldLabel>
+                    <PasswordInput
+                      id="password"
+                      placeholder="••••••••"
+                      {...form.register("password")}
+                    />
+                    {form.formState.errors.password && (
+                      <p className="text-sm text-destructive">
+                        {form.formState.errors.password.message as string}
+                      </p>
+                    )}
+                  </Field>
+
+                  <div className="flex justify-end">
+                    <Button
+                      type="submit"
+                      className="w-full cursor-pointer"
+                      disabled={isPending}
+                    >
+                      {isPending ? (
+                        <span className="flex items-center justify-center gap-2">
+                          <Loader2 className="h-4 w-4 animate-spin" />
+                          Signing up...
+                        </span>
+                      ) : (
+                        "Sign Up"
+                      )}
+                    </Button>
+                  </div>
+
+                  <div className="text-center text-sm">
+                    Already have an account?{" "}
+                    <Link
+                      href="/login"
+                      className="underline underline-offset-4"
+                    >
+                      Log in
+                    </Link>
+                  </div>
+                </div>
+              </FieldSet>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
